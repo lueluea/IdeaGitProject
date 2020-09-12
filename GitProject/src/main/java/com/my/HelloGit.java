b25e81d01e40e65e499abf2f314224ede1692a61
zqx <1277145792@qq.com>
2020-09-12 17:31:44 +0800

    更新分支

--- GitProject/src/main/java/com/my/HelloGit.java
+++ GitProject/src/main/java/com/my/HelloGit.java
@@ -8,5 +8,11 @@ public class HelloGit
         System.out.println("更新1");
         System.out.println("更新分支！");
         System.out.println("分支添加！");
+        System.out.println("更新分支");
+    }
+
+    public static void test()
+    {
+
     }
 }
