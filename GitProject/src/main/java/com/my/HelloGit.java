fb071372e093cd6bf511042093224e8de3a53730
zqx <1277145792@qq.com>
2020-09-12 19:27:47 +0800

    克隆者更新

--- GitProject/src/main/java/com/my/HelloGit.java
+++ GitProject/src/main/java/com/my/HelloGit.java
@@ -10,6 +10,7 @@ public class HelloGit
         System.out.println("分支添加！");
         System.out.println("更新分支");
         System.out.println("在线更新");
+        System.out.println("克隆者更新！！");
     }
 
     public static void test()
